7ba32eeddd84ab501f44f8c5fcece57698282909
Yusifov Rustam <noirtxt@gmail.com>
2024-06-25 20:36:18 +0400

    init

---
+++ dist/index.672d4772.js
@@ -0,0 +1,155 @@
+"use strict";
+// Hacky hacks start here (tnx parcel)
+const placeholderUrl = document.getElementById("img-placeholder").src;
+const agentpinUrl = document.getElementById("img-agentpin").src;
+const petpinUrl = document.getElementById("img-petpin").src;
+const shelpinUrl = document.getElementById("img-shelpin").src;
+const myIcon = L.icon({
+    iconUrl: placeholderUrl,
+    iconSize: [
+        60,
+        60
+    ],
+    iconAnchor: [
+        30,
+        60
+    ],
+    popupAnchor: [
+        -3,
+        -60
+    ]
+});
+const petIcon = L.icon({
+    iconUrl: petpinUrl,
+    iconSize: [
+        60,
+        60
+    ],
+    iconAnchor: [
+        30,
+        59
+    ],
+    popupAnchor: [
+        -3,
+        -60
+    ]
+});
+const shelIcon = L.icon({
+    iconUrl: shelpinUrl,
+    iconSize: [
+        60,
+        60
+    ],
+    iconAnchor: [
+        30,
+        59
+    ],
+    popupAnchor: [
+        -3,
+        -60
+    ]
+});
+const shelters = {
+    tamaz: [
+        41.68058616424739,
+        44.890587433797606
+    ],
+    saburtalo: [
+        41.759052910070956,
+        44.7241872267311
+    ],
+    vake: [
+        41.77725673725068,
+        44.734871839005876
+    ],
+    varketili: [
+        41.68200580031455,
+        44.87386397170825
+    ]
+};
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--typeuc");
+const inputSos = document.querySelector(".form__input--typesos");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--temp");
+const inputElevation = document.querySelector(".form__input--climb");
+const agentstvo = document.querySelector(".form__sublabel__agent");
+const checkbox = document.querySelector(".form__check");
+class App {
+    #map;
+    #mapEvent;
+    constructor(){
+        this._get_Position();
+        form.addEventListener("submit", this._newWorkout.bind(this));
+        inputType.addEventListener("change", function() {
+            agentstvo.classList.toggle("hidden");
+        });
+        inputSos.addEventListener("change", function() {
+            checkbox.classList.toggle("hidden");
+        });
+        checkbox.addEventListener("change", this._showShelters.bind(this));
+    }
+    _get_Position() {
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this));
+    }
+    _loadMap(position) {
+        const coords = [
+            position.coords.latitude,
+            position.coords.longitude
+        ];
+        this.#map = L.map("map").setView(coords, 13);
+        L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+            maxZoom: 20,
+            subdomains: [
+                "mt0",
+                "mt1",
+                "mt2",
+                "mt3"
+            ]
+        }).addTo(this.#map);
+        L.marker(coords, {
+            icon: myIcon
+        }).addTo(this.#map).bindPopup("\u0412\u044B \u043D\u0430\u0445\u043E\u0434\u0438\u0442\u0435\u0441\u044C \u0442\u0443\u0442").openPopup();
+        this.#map.on("click", this._showForm.bind(this));
+    }
+    _showForm(e) {
+        this.#mapEvent = e;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+    }
+    _showHelpNumber() {}
+    _showShelters() {
+        L.marker(shelters.tamaz, {
+            icon: shelIcon
+        }).addTo(this.#map).bindPopup("\u0420\u0430\u0439\u043E\u043D \u0422\u0430\u043C\u0430\u0437. \u0420\u0430\u0431\u043E\u0442\u0430\u0435\u0442 \u043A\u0440\u0443\u0433\u043B\u043E\u0441\u0443\u0442\u043E\u0447\u043D\u043E. \u0422\u0435\u043B: 595 63 36 36").openPopup();
+        L.marker(shelters.vake, {
+            icon: shelIcon
+        }).addTo(this.#map).bindPopup("\u0420\u0430\u0439\u043E\u043D \u0412\u0430\u043A\u0435. \u0420\u0430\u0431\u043E\u0442\u0430\u0435\u0442 \u0434\u043E 19:00. \u0422\u0435\u043B: 595 63 36 36").openPopup();
+        L.marker(shelters.varketili, {
+            icon: shelIcon
+        }).addTo(this.#map).bindPopup("\u0420\u0430\u0439\u043E\u043D \u0412\u0430\u0440\u043A\u0435\u0442\u0438\u043B\u0438. \u0420\u0430\u0431\u043E\u0442\u0430\u0435\u0442 \u043A\u0440\u0443\u0433\u043B\u043E\u0441\u0443\u0442\u043E\u0447\u043D\u043E. \u0422\u0435\u043B: 595 63 36 36").openPopup();
+        L.marker(shelters.saburtalo, {
+            icon: shelIcon
+        }).addTo(this.#map).bindPopup("\u0420\u0430\u0439\u043E\u043D \u0421\u0430\u0431\u0443\u0440\u0442\u0430\u043B\u043E. \u0420\u0430\u0431\u043E\u0442\u0430\u0435\u0442 \u043A\u0440\u0443\u0433\u043B\u043E\u0441\u0443\u0442\u043E\u0447\u043D\u043E. \u0422\u0435\u043B: 595 63 36 36").openPopup();
+    }
+    _newWorkout(ev) {
+        ev.preventDefault();
+        const pos = [
+            this.#mapEvent.latlng.lat,
+            this.#mapEvent.latlng.lng
+        ];
+        L.marker(pos, {
+            icon: petIcon
+        }).addTo(this.#map).bindPopup(L.popup({
+            content: "\u0412\u0430\u0448\u0430 \u0442\u043E\u0447\u043A\u0430",
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup"
+        })).openPopup();
+    }
+}
+const app = new App();
+
+//# sourceMappingURL=index.672d4772.js.map
